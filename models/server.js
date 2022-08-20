8eafd41f33b3a0831e94e5f042e4b292c1809a12
Angel Omar Saavedra <angel.saaveda@gmail.com>
2022-08-20 13:58:36 -0300

    cors

--- models/server.js
+++ models/server.js
@@ -6,6 +6,7 @@ const http = require('http');
 const io = require('socket.io');
 const path = require('path');
 const Sockets = require('./sockets');
+const cors = require('cors');
 
 class Server {
     constructor(){
@@ -21,7 +22,11 @@ class Server {
     }
 
     middlewares(){
+        // desplegar el directorio publico
         this.app.use( express.static( path.resolve( __dirname, '../public') ) );
+
+        // CORS
+        this.app.use( cors() );
     }
 
     configurarSockets(){
